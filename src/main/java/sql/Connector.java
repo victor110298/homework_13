eb062ee3f6574c9dd9a4caf744ead2f81bbe3167
victor110298 <44604893+victor110298@users.noreply.github.com>
2019-04-11 17:32:52 +0300

    jdbc

---
+++ src/main/java/sql/Connector.java
@@ -0,0 +1,28 @@
+package sql;
+
+import org.apache.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Connector {
+    private static final Logger LOGGER = Logger.getLogger(Connector.class);
+
+    public static Connection getConnection() {
+        Properties property = new Properties();
+        Connection connection = null;
+        try {
+            FileInputStream fis = new FileInputStream("src/main/resources/database/properties");
+            property.load(fis);
+            connection = DriverManager.getConnection(property.getProperty("url"),
+                    property.getProperty("user"), property.getProperty("password"));
+        } catch (SQLException | IOException e) {
+            LOGGER.error(e);
+        }
+        return connection;
+    }
+}
